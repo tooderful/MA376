a9695c6e12f94515ae4f9aac8c6a7bec39197349
Anny-Claude Joseph <dr.aclaudej@gmail.com>
2020-09-02 22:04:03 -0400

    Create CEPR_2018_Data3.R

---
+++ LSN 6/Prep Files/CEPR_2018_Data3.R
@@ -0,0 +1,28 @@
+# Requires CEPR_2018_Data.csv
+
+# Include packages
+packages <- c('tidyverse', 'table1')
+lapply(packages, library, character.only = TRUE)
+#-------------------------------------------------------------------------------------------------------------------------------#
+dat <- read_csv('C:/Users/annyclaude.joseph/Desktop/MA376/CEPR2018.csv')
+
+wage.dat <- dat %>% filter(Income > 0 & 
+                              Income <= 150000 & 
+                              Education == 'Bachelors')  
+ 
+
+set.seed(356)
+female.dat <- wage.dat %>% filter(Sex == 'F') 
+female.dat <- sample_n(female.dat, 700)
+
+male.dat <- wage.dat %>% filter(Sex == 'M') 
+male.dat <- sample_n(male.dat, 700)
+
+wage.dat3 <- rbind(male.dat, female.dat)
+
+write.csv(wage.dat3, 'C:/Users/annyclaude.joseph/Desktop/MA376/CEPR2018_3.csv')
+#### End
+#-------------------------------------------------------------------------------------------------------------------------------#
+
+
+ 
